03d9bf6bfce81a6eaa944742b0ddc233d3aba343
sechaser <413406236@qq.com>
2016-09-03 17:04:52 +0800

    Implement the full arrangement of a string using recursion

--- main.cpp
+++ main.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
-#include <vector>
+#include <string>
 
-#define target 26
+#include <assert.h>
+
+static int count = 1;
+void swap(std::string& str, int xpos, int ypos)
+{
+    assert((xpos >= 0) && (xpos < str.size()));
+    assert((ypos >= 0) && (ypos < str.size()));
+
+    char temp = str[xpos];
+    str[xpos] = str[ypos];
+    str[ypos] = temp;
+}
+
+void fullArrange(std::string str, int beg, int end)
+{
+    if(beg == end)
+    {
+        std::cout<<"The "<<count<<" arrangement is "<<str<<std::endl;
+        ++ count;
+    }
+    else
+    {
+        for(int i = beg; i != end; ++ i)
+        {
+            swap(str, beg, i);
+            fullArrange(str, beg + 1, end);
+            swap(str, beg, i);
+        }
+    }
+}
 
 int main()
 {
-    std::cout<<"Hello World"<<std::endl;
+    std::string test = "test";
+    fullArrange(test, 0, test.size());
 
     return 0;
 }
